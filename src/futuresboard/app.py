c53c07f2c829b5f4a9150790c13ce7eb6c57bb09
Pedro Algarvio <pedro@algarvio.me>
2021-12-13 07:39:22 +0000

    Let pydantic do the validation for us

--- src/futuresboard/app.py
+++ src/futuresboard/app.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import json
 import logging
 import pathlib
-from typing import Any
 
 from flask import Flask
 from flask import redirect
@@ -12,6 +11,7 @@ from flask import request
 import futuresboard.scraper
 from futuresboard import blueprint
 from futuresboard import db
+from futuresboard.config import Config
 
 
 def clear_trailing():
@@ -20,78 +20,18 @@ def clear_trailing():
         return redirect(rp[:-1])
 
 
-def init_app(config: dict[str, Any] | None = None):
+def init_app(config: Config | None = None):
     if config is None:
-        config_dir = pathlib.Path.cwd()
-        config = {"CONFIG_DIR": config_dir, "DISABLE_AUTO_SCRAPE": False}
-
-        config_file = config_dir / "config.json"
-        for key, value in json.loads(config_file.read_text()).items():
-            if key == "database":
-                value = pathlib.Path(value).resolve()
-            config[key.upper()] = value
-
-        if "DATABASE" not in config:
-            config["DATABASE"] = f"{config_dir / 'futures.db'}"
-
-        if "EXCHANGE" not in config:
-            base_url = "https://fapi.binance.com"
-        else:
-            if config["EXCHANGE"] == "binance":
-                base_url = "https://fapi.binance.com"
-            else:
-                base_url = "https://fapi.binance.com"  # alternatives here later
-
-        config["API_BASE_URL"] = base_url
-
-        if "AUTO_SCRAPE_INTERVAL" not in config:
-            config["AUTO_SCRAPE_INTERVAL"] = 5 * 60
-        else:
-            try:
-                interval = int(config["AUTO_SCRAPE_INTERVAL"])
-                if interval < 60 or interval > 3600:
-                    config["AUTO_SCRAPE_INTERVAL"] = 5 * 60
-            except TypeError:
-                config["AUTO_SCRAPE_INTERVAL"] = 5 * 60
-
-        if "CUSTOM" not in config:
-            config["CUSTOM"]["NAVBAR_TITLE"] = "Futuresboard"
-            config["CUSTOM"]["NAVBAR_BG"] = "bg-dark"
-            config["CUSTOM"]["PROJECTIONS"] = [1.003, 1.005, 1.01, 1.012]
-        else:
-            if len(config["CUSTOM"]["NAVBAR_TITLE"]) < 1 or len(config["NAVBAR_TITLE"]) > 50:
-                config["CUSTOM"]["NAVBAR_TITLE"] = "Futuresboard"
-            if config["CUSTOM"]["NAVBAR_BG"] not in [
-                "bg-primary",
-                "bg-secondary",
-                "bg-success",
-                "bg-danger",
-                "bg-warning",
-                "bg-info",
-                "bg-light",
-            ]:
-                config["CUSTOM"]["NAVBAR_BG"] = "bg-dark"
-            if not isinstance(config["CUSTOM"]["PROJECTIONS"], list):
-                config["CUSTOM"]["PROJECTIONS"] = [1.003, 1.005, 1.01, 1.012]
-            else:
-                temp = []
-                for percentage in config["CUSTOM"]["PROJECTIONS"]:
-                    try:
-                        percentage = float(percentage)
-                        if percentage > -3.0 and percentage < 3.0:
-                            temp.append(percentage)
-                    except TypeError:
-                        pass
-                config["CUSTOM"]["PROJECTIONS"] = temp
+        config = Config.from_config_dir(pathlib.Path.cwd())
 
     app = Flask(__name__)
-    app.config.from_mapping(**config)
+    app.config.from_mapping(**json.loads(config.json()))
     app.url_map.strict_slashes = False
     db.init_app(app)
     app.before_request(clear_trailing)
     app.register_blueprint(blueprint.app)
 
-    if config["DISABLE_AUTO_SCRAPE"] is False:
+    if config.DISABLE_AUTO_SCRAPE is False:
         futuresboard.scraper.auto_scrape(app)
 
     app.logger.setLevel(logging.INFO)
--- src/futuresboard/cli.py
+++ src/futuresboard/cli.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import argparse
-import json
 import logging
 import pathlib
 import sys
@@ -9,6 +8,7 @@ import sys
 import futuresboard.app
 import futuresboard.scraper
 from futuresboard import __version__  # type: ignore[attr-defined]
+from futuresboard.config import Config
 
 log = logging.getLogger(__name__)
 
@@ -33,37 +33,22 @@ def main():
         help="Disable the routines which scrape while the webservice is running",
     )
     server_settings = parser.add_argument_group("Server Settings")
-    server_settings.add_argument("--host", default="0.0.0.0", help="Server host. Default: 0.0.0.0")
+    server_settings.add_argument("--host", default=None, help="Server host. Default: 0.0.0.0")
     server_settings.add_argument(
-        "--port", type=int, default=5000, help="Server port. Default: 5000"
+        "--port", type=int, default=None, help="Server port. Default: 5000"
     )
     args = parser.parse_args()
 
-    config = {}
     if args.config_dir is None:
         args.config_dir = pathlib.Path.cwd() / "config"
     else:
         args.config_dir = args.config_dir.resolve()
-    config["CONFIG_DIR"] = args.config_dir
 
-    config_file = args.config_dir / "config.json"
-    for key, value in json.loads(config_file.read_text()).items():
-        if key == "database":
-            value = pathlib.Path(value).resolve()
-        config[key.upper()] = value
-
-    if "DATABASE" not in config:
-        config["DATABASE"] = f"{args.config_dir / 'futures.db'}"
-
-    if "API_BASE_URL" not in config:
-        base_url = "https://fapi.binance.com"  # production base url
-        # base_url = 'https://testnet.binancefuture.com' # testnet base url
-        config["API_BASE_URL"] = base_url
-
-    if "AUTO_SCRAPE_INTERVAL" not in config:
-        config["AUTO_SCRAPE_INTERVAL"] = 5 * 60
-
-    config["DISABLE_AUTO_SCRAPE"] = args.disable_auto_scraper
+    config = Config.from_config_dir(args.config_dir)
+    if not args.host:
+        args.host = config.HOST
+    if not args.port:
+        args.port = config.PORT
 
     # Run the application
     app = futuresboard.app.init_app(config)
