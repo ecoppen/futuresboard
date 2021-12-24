a0c5411d5dba7d2ce7d7deb24a93ab2e0014e64a
ecoppen <51025241+ecoppen@users.noreply.github.com>
2021-12-24 10:13:55 +0000

    Update chartjs-chart-financial.js
    Change chart opaciy to 0.6

--- src/futuresboard/static/js/chartjs-chart-financial.js
+++ src/futuresboard/static/js/chartjs-chart-financial.js
@@ -254,9 +254,9 @@ const globalOpts$2 = chart_js.Chart.defaults;
 
 globalOpts$2.elements.financial = {
 	color: {
-		up: 'rgba(80, 160, 115, 1)',
-		down: 'rgba(215, 85, 65, 1)',
-		unchanged: 'rgba(90, 90, 90, 1)',
+		up: 'rgba(80, 160, 115, 0.6)',
+		down: 'rgba(215, 85, 65, 0.6)',
+		unchanged: 'rgba(90, 90, 90, 0.6)',
 	}
 };
 
