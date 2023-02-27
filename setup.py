5ea487fb0b7d0e39f09dd937fcec71a9e4accca0
Ed Coppen <51025241+ecoppen@users.noreply.github.com>
2023-02-27 07:39:37 +0000

    Delete v1 files

--- setup.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-
-import setuptools
-
-if __name__ == "__main__":
-    setuptools.setup(use_scm_version=True)
--- src/futuresboard/wsgi.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-
-import futuresboard.app
-
-
-app = futuresboard.app.init_app()
