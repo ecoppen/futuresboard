bde07e3fad90cc665d467d9cbb8eb3b9d871311e
Ed Coppen <51025241+ecoppen@users.noreply.github.com>
2023-02-27 16:50:51 +0000

    initial endpoints

--- futuresboard/main.py
+++ futuresboard/main.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import logging
 import os
 import threading
@@ -9,6 +11,9 @@ from fastapi.middleware.gzip import GZipMiddleware
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
+from futuresboard.exchange.factory import load_exchanges
+from futuresboard.exchange.utils import Exchanges, Intervals, Markets
 
 logs_file = Path(Path().resolve(), "log.txt")
 logs_file.touch(exist_ok=True)
@@ -27,3 +32,54 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.add_middleware(GZipMiddleware)
 templates = Jinja2Templates(directory="templates")
+
+exchanges = load_exchanges()
+
+
+@app.get("/getprice")
+def get_price(
+    exchange: Exchanges,
+    market: Markets,
+    base: str,
+    quote: str,
+):
+    if market == Markets.FUTURES.value:
+        return exchanges[exchange].get_futures_price(
+            base=base.upper(), quote=quote.upper()
+        )
+
+    return {"error": "not implemented yet"}
+
+
+@app.get("/getprices")
+def get_prices(
+    exchange: Exchanges,
+    market: Markets,
+):
+    if market == Markets.FUTURES.value:
+        return exchanges[exchange].get_futures_prices()
+    return {"error": "not implemented yet"}
+
+
+@app.get("/getkline")
+def get_kline(
+    exchange: Exchanges,
+    market: Markets,
+    base: str,
+    quote: str,
+    interval: Intervals = Intervals.ONE_DAY,
+    start_time: int | None = None,
+    end_time: int | None = None,
+    limit: int = 500,
+):
+    if market == Markets.FUTURES.value:
+        return exchanges[exchange].get_futures_kline(
+            base=base.upper(),
+            quote=quote.upper(),
+            interval=interval,
+            start_time=start_time,
+            end_time=end_time,
+            limit=limit,
+        )
+    else:
+        return {"error": "not implemented yet"}
