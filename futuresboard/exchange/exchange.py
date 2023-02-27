db2eaa0ff10fdd714b3640f6cb21a0631e909808
Ed Coppen <51025241+ecoppen@users.noreply.github.com>
2023-02-27 16:51:16 +0000

    Create exchange.py

---
+++ futuresboard/exchange/exchange.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+import logging
+import time
+from decimal import Decimal
+
+from futuresboard.exchange.utils import Intervals
+
+log = logging.getLogger(__name__)
+
+
+class Exchange:
+    def __init__(self):
+        pass
+
+    exchange: str | None = None
+    futures_api_url: str | None = None
+    futures_trade_url: str | None = None
+    weight: int = 0
+    max_weight: int = 100
+
+    def check_weight(self) -> None:
+        if self.weight > self.max_weight:
+            log.info(
+                f"Weight {self.weight} is greater than {self.max_weight}, sleeping for 60 seconds"
+            )
+            time.sleep(60)
+
+    def update_weight(self, weight: int) -> None:
+        self.weight = weight
+
+    def get_futures_price(self, base: str, quote: str) -> Decimal:
+        return Decimal(-1.0)
+
+    def get_futures_prices(self) -> list:
+        return []
+
+    def get_futures_kline(
+        self,
+        base: str,
+        quote: str,
+        start_time: int,
+        end_time: int | None = None,
+        interval: Intervals = Intervals.ONE_DAY,
+        limit: int = 500,
+    ) -> list:
+        return []
+
+    def get_futures_trade_url(self):
+        return self.futures_trade_url
